27d458af29bceeec0d36f7bf80d614912b2ec8c5
Shibil c <81413838+iamshibilc@users.noreply.github.com>
2021-06-28 22:05:15 +0530

    Add files via upload

---
+++ Kalimayam.py
@@ -0,0 +1,23 @@
+import os
+print("\033[1;30;40m \n")
+os.system("clear")
+print ("  TOOL CREATED BY SIMZEOVER")
+print (" ")
+print (""" \033[1;31;40m  ████████  ██████████████   """)
+print (""" \033[1;30;40m     ██     ██    ██    ██    """)
+print (""" \033[1;30;40m     ██     ██    ██    ██    """)
+print (""" \033[1;30;40m     ██     ██    ██    ██    """)
+print (""" \033[1;30;40m     ██     ██          ██       """)
+
+print (" ")
+print (" ")
+print ("  1.HIGH PERFORMANCE")
+print ("  2.LOW PERFORMANCE")
+team = int(input ("""  >>>"""))
+if team ==1:
+ os.system("clear")
+ os.system("bash mainh.sh")
+elif team==2:
+ os.system("clear")
+ os.system("bash mainl.sh")
+
